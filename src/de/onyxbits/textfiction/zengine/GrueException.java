bad466740ca45326bde7e7364ad726590e2a74f0
patrick <github@onyxbits.de>
2014-01-01 03:45:25 +0100

    Forgot file

---
+++ src/de/onyxbits/textfiction/zengine/GrueException.java
@@ -0,0 +1,29 @@
+package de.onyxbits.textfiction.zengine;
+
+public class GrueException extends RuntimeException {
+
+	/**
+	 * Thrown when the Z-Code is somehow messed up.
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public GrueException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public GrueException(String detailMessage) {
+		super(detailMessage);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GrueException(Throwable throwable) {
+		super(throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GrueException(String detailMessage, Throwable throwable) {
+		super(detailMessage, throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+}
